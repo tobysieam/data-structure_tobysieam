f486358f3be840d6699f7a039e82206b871b39d7
tobysieam <tobysieam@hotmail.com>
2024-11-07 17:58:25 +0800

    队列（链式存储）完

--- 队列（链式存储）.c
+++ 队列（链式存储）.c
@@ -23,7 +23,6 @@ typedef struct
 
 } LinkQueue;
 
-
 // 3、链队初始化
 // 构造一个空的队列，既创建一个链队结点，将front和rear域都设置为NULL
 LinkQueue *InitQueue()
@@ -36,12 +35,11 @@ LinkQueue *InitQueue()
 }
 // LinkQueue lq = *InitQueue();
 
-
-//4、判断队列是否为空
-//判断q->front == NULL && q->rear == NULL的条件成立即可
-int QueueEmpty(LinkQueue* q)
+// 4、判断队列是否为空
+// 判断q->front == NULL && q->rear == NULL的条件成立即可
+int QueueEmpty(LinkQueue *q)
 {
-    if(q->front == NULL && q->rear == NULL)
+    if (q->front == NULL && q->rear == NULL)
     {
         return 1;
     }
@@ -51,26 +49,142 @@ int QueueEmpty(LinkQueue* q)
     }
 }
 
-
-//5、入队
-void enQueue(LinkQueue* q,ElemType e)
+// 5、入队
+void enQueue(LinkQueue *q, ElemType e)
 {
-    DataNode* t;
-    //构造一个节点t，data域存储e，next域存储NULL
-    t = (DataNode*)malloc(sizeof(DataNode));
+    DataNode *t;
+    // 构造一个节点t，data域存储e，next域存储NULL
+    t = (DataNode *)malloc(sizeof(DataNode));
     t->data = e;
     t->next = NULL;
-    //添加
-    if(q->front != NULL||q->rear != NULL)
+    // 添加
+    if (q->front != NULL || q->rear != NULL)
     {
-        //队非空
+        // 队非空
         q->rear->next = t;
         q->rear = t;
     }
     else
     {
-        //队空
+        // 队空
         q->front = t;
         q->rear = t;
     }
+}
+
+// 6、出队
+/*
+若队列非空，出队，返回1，否则，提示，返回0
+*/
+int deQueue(LinkQueue *q, ElemType *e)
+{
+    DataNode *t;
+
+    if (q->front != NULL || q->rear != NULL) // 非空
+    {
+        // 1、让t指向队头元素节点
+        t = q->front;
+        // 2、把队头元素存储到e
+        *e = t->data;
+        // 3、删除队头元素节点
+        if (q->front->next == NULL) // 只有1个元素
+        {
+            // 只有一个元素
+            q->front = NULL;
+            q->rear = NULL;
+        }
+        else
+        {
+            // 多于一个元素
+            q->front = t->next;
+        }
+        // 4、释放t所占的内存空间
+        free(t);
+        return 1;
+    }
+    else
+    {
+        printf("队空，不能出队\n");
+        return 0;
+    }
+}
+
+// 7、求队列长度
+int lengthLinkQueue(LinkQueue *q)
+{
+    int len;
+    if (QueueEmpty(&q)) // 判断队列是否为空
+    {
+        len = 0;
+        return len;
+    }
+
+    DataNode *t;
+    // 构造一个节点t，让它指向队首元素front
+    t = (DataNode *)malloc(sizeof(DataNode));
+    t = q->front;
+    len = 1;
+    while (t->next != NULL)
+    {
+        len++;
+        t = t->next;
+    }
+    printf("队列长度为%d\n", len);
+}
+
+// 8、打印队列中的元素
+void DispQueue(LinkQueue *q)
+{
+    DataNode *p;
+    p = q->front;
+    printf("队列元素为：");
+    while (p != NULL)
+    {
+        printf("%d", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+// 9、销毁队列
+void DestroyQueue(LinkQueue *q)
+{
+    int deQueue(LinkQueue * q, ElemType * e);
+    ElemType m;
+
+    while (q->front != NULL || q->rear != NULL)
+    {
+        // 出队
+        deQueue(q, &m);
+    }
+    free(q);
+}
+
+// 测试代码
+int main()
+{
+    LinkQueue lq = *InitQueue();
+    enQueue(&lq, 1);
+    enQueue(&lq, 2);
+    enQueue(&lq, 3);
+    enQueue(&lq, 4);
+    enQueue(&lq, 5);
+    enQueue(&lq, 6);
+    enQueue(&lq, 7);
+    enQueue(&lq, 8);
+    enQueue(&lq, 9);
+    DispQueue(&lq);
+    lengtthLinkQueue(&lq);
+
+    ElemType *e;
+    e = (ElemType *)malloc(sizeof(ElemType));
+    deQueue(&lq, e);
+    printf("出队元素：‰d\n", *e);
+    deQueue(&lq, e);
+    printf("出队元素：%d\n", *e);
+    printf("队列为空返回1，否则返回0：%dn", QueueEmpty(&lq));
+    DispQueue(&lq);
+    lengthLinkQueue(&lq);
+    DestroyQueue(&lq); // 销毁队列
+    return 1;
 }
